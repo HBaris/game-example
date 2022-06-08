99affa9b08eb7faffdc5467289aeffaf0cee2467
HBado <nes77850@gmail.com>
2022-06-08 17:51:24 +0300

    Commit to main

---
+++ Engellerden kaçma 2D/Assets/2DBall/Engeller/Char_Movement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Char_Movement : MonoBehaviour
+{
+    public 
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
